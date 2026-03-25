8fe8e7c6afb99675d22c41f964b07e68445ac48a
Gustvx-619 <guapim02@gmail.com>
2026-03-25 16:21:42 -0500

    SELECT INTERPRETER
    En el desktop de GUSTAVO al querer revisar el linear regression y el APP, no permite por Flask, se debe hacer borre e instalacion

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request
+from Flask import Flask, render_template, request
 import LinearRegression
 
-app =Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def home():
